7275538155badbdbb3e1a2922c75b59efb31cde9
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-07 23:52:11 -0800

    28 ms

---
+++ solutions/100. Same Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Time - O(n) where n is the size of the tree
+    # Space - O(d) where d is the depth of the tree
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        else:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            
